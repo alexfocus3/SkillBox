489888885bdcfb072ef5300c22fd7828e83a3be3
alexfocus3 <157527574+alexfocus3@users.noreply.github.com>
2024-01-24 22:24:32 +0300

    Add files via upload
    Напишите программу, которая на вход получала бы строку, введённую пользователем, а в файл писала № строки, дату и сообщение в формате: 2020-02-10 15:00:00 продам гараж. При вводе слова exit программа завершает работу.

---
+++ task1.go
@@ -0,0 +1,32 @@
+//Напишите программу, которая на вход получала бы строку, введённую пользователем,
+//а в файл писала № строки, дату и сообщение в формате: 2020-02-10 15:00:00 продам гараж.
+//При вводе слова exit программа завершает работу.
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	var str string
+	var count int = 1
+
+	file, err := os.Create("res.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+	for {
+		fmt.Print("Введите строку: ")
+		fmt.Fscan(os.Stdin, &str)
+		if str == "exit" {
+			break
+		} else {
+			fmt.Fprintf(file, "%d %s %s\n", count, time.Now().Format("01-02-2006 15:04:05"), "продам гараж")
+		}
+		count++
+	}
+}
